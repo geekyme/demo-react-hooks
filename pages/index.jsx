baa1f15d3551fce25a292b1f018454fddec80321
Shawn Lim <shawnlim89@live.com>
2019-03-17 16:58:04 +0800

    Use regular form config

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,22 +5,24 @@ import FormInput from "components/FormInput";
 import getFormConfig, { getLargeConfig } from "./getFormConfig";
 
 export default function AgendaPage() {
-  const config = getLargeConfig();
-  const initialState = {};
+  // const config = getLargeConfig();
+  // const initialState = {};
 
-  Object.keys(config.nodes).forEach(k => {
-    initialState[k] = "";
-  });
+  // Object.keys(config.nodes).forEach(k => {
+  //   initialState[k] = "";
+  // });
 
-  // const initialState = {
-  //   one: "hello",
-  //   two: "world",
-  //   check_1: false,
-  //   check_2: false,
-  //   check_3: true,
-  //   radio_1: "Singaporean / PR",
-  //   identity_input: ""
-  // };
+  const initialState = {
+    one: "hello",
+    two: "world",
+    check_1: false,
+    check_2: false,
+    check_3: true,
+    radio_1: "Singaporean / PR",
+    identity_input: ""
+  };
+
+  const config = getFormConfig();
 
   function standaloneOnChange(e) {
     console.log("standalone", e.target.value);
