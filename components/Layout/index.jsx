b7fd85fde9f899903e0e60524feab07cf5fbabc9
Shawn Lim <shawnlim89@live.com>
2019-04-01 23:07:51 +0800

    Add a subtitle

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,11 +1,18 @@
 import "reset-css";
 import styles from "./Layout.scss";
 import { H1 } from "components/Typography";
+import { TiArrowRight } from "react-icons/ti";
 
 export default function Layout(props) {
   return (
     <div className={styles.bg}>
-      <H1 className={styles.title}>Demo React Hooks</H1>
+      <div className={styles.header}>
+        <H1 className={styles.title}>Demo React Hooks</H1>
+        <p className={styles.subtitle}>
+          Check out the source code{" "}
+          <TiArrowRight className={styles.arrowIcon} />
+        </p>
+      </div>
       <div className={styles.tablet}>
         <div className={styles.screen}>{props.children}</div>
       </div>
