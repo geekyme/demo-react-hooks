2144da139357a80718baaca11070e856039e7627
Shawn Lim <shawnlim89@live.com>
2019-04-05 02:11:32 +0800

    Add coverage to pipeline

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,12 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  },
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 90,
+      lines: 90
+    }
   }
 };
