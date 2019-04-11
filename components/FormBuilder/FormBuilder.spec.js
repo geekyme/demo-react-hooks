16914d741b3e80bf16d1c71b2dc96abf6f0474e5
Shawn Lim <shawnlim89@live.com>
2019-04-11 22:26:54 +0800

    Cover test for initial rendering with relationships executed

--- components/FormBuilder/FormBuilder.spec.js
+++ components/FormBuilder/FormBuilder.spec.js
@@ -18,10 +18,14 @@ describe("<FormBuilder />", () => {
   afterEach(cleanup);
 
   describe("Initial state rendering", () => {
-    it("shows initial state", () => {
+    it("shows initial state with relationships executed", () => {
       const id_no = wrapper.getByTestId("id_no");
+      const id_no_copy = wrapper.getByTestId("id_no_copy");
+      const id_no_another_copy = wrapper.getByTestId("id_no_another_copy");
 
       expect(id_no.value).toEqual(defaultState.id_no);
+      expect(id_no_copy.value).toEqual(defaultState.id_no);
+      expect(id_no_another_copy.value).toEqual(defaultState.id_no);
     });
   });
   describe("Submitting without filling form", () => {
@@ -33,7 +37,6 @@ describe("<FormBuilder />", () => {
 
       wrapper.getByText("Please select a valid to and from date");
       wrapper.getByText("Enter a proper NRIC no!");
-      wrapper.getByText("This is required!");
     });
 
     it("shows all form errors", () => {
@@ -43,7 +46,6 @@ describe("<FormBuilder />", () => {
 
       wrapper.getByText("Please select a valid to and from date");
       wrapper.getByText("Enter a proper NRIC no!");
-      wrapper.getByText("This is required!");
     });
   });
 
